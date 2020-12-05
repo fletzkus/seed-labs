1afaa53355722bd307f879d17dc7a01a8adbab82
Collin Howland <collin.howland@hotmail.com>
2020-12-05 14:04:06 -0800

    commented out malicious button css and deleted unnecessary test stuff

--- category-web/Web_Clickjacking/server.py
+++ category-web/Web_Clickjacking/server.py
@@ -2,26 +2,6 @@ from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, yeet!"
-
-# @app.route("/malicious")
-# def malicious():
-#     resp = Response()
-#     resp.data = render_template('malicious.html')
-#     # resp = Flask.make_response(render_template('index.html', title="Welcome", username="Stanley"))
-#     return resp
-
-# @app.route("/safe_malicious")
-# def safe_malicious():
-#     resp = Response()
-#     resp.data = render_template('malicious.html')
-#     # resp = Flask.make_response(render_template('index.html', title="Welcome", username="Stanley"))
-#     resp.headers['X-Frame-Options'] = 'DENY'
-#     resp.headers['Content-Security-Policy'] = "frame-ancestors 'none'"
-#     return resp
-
 @app.route("/not_malicious")
 def not_malicious():
     resp = Response()
@@ -32,14 +12,3 @@ def not_malicious():
     # resp.headers['Content-Security-Policy'] = "frame-ancestors 'none'"
 
     return resp
-
-@app.route("/clicked_button")
-def clicked_button():
-    resp = Response()
-    resp.headers['X-Frame-Options'] = 'DENY'
-    resp.headers['Content-Security-Policy'] = "frame-ancestors 'none'"
-    return resp
-
-@app.route("/test")
-def test():
-    return "Hello, test!"
