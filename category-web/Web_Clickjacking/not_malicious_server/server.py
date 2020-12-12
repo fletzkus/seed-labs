c4feb73e5f96b808f7a0d6cf87566ddab63592d3
faithletzkus <fletzkus@wustl.edu>
2020-12-12 13:03:09 -0600

    removing sln code

--- category-web/Web_Clickjacking/not_malicious_server/server.py
+++ category-web/Web_Clickjacking/not_malicious_server/server.py
@@ -7,8 +7,6 @@ def not_malicious():
     resp = Response()
     resp.data = render_template('not_malicious.html')
 
-    # Add in for safety
-    # resp.headers['X-Frame-Options'] = 'DENY'
-    # resp.headers['Content-Security-Policy'] = "frame-ancestors 'none'"
+    # TODO: Task 5
 
     return resp
